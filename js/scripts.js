d2f316c7d1413e33da4afb9d67efcb24e66c6ca5
SungmiKim <62002100+SungmiKim@users.noreply.github.com>
2020-07-22 14:37:49 +0900

    2020-07-22
    2020-07-22

--- js/scripts.js
+++ js/scripts.js
@@ -1,89 +1,96 @@
 $(window).ready(function () {
-	var typingBool = false;
-	var typingIdx = 0;
-	var liIndex = 0;
-	var liLength = $(".typing-txt>ul>li").length;
+  var typingBool = false;
+  var typingIdx = 0;
+  var liIndex = 0;
+  var liLength = $(".typing-txt>ul>li").length;
 
-	var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
-	typingTxt = typingTxt.split("");
-	if (typingBool == false) {
-		typingBool = true;
-		var tyInt = setInterval(typing, 100);
-	}
+  var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
+  typingTxt = typingTxt.split("");
+  if (typingBool == false) {
+    typingBool = true;
+    var tyInt = setInterval(typing, 100);
+  }
 
-	function typing() {
-		if (typingIdx < typingTxt.length) {
-			$(".typing").append(typingTxt[typingIdx]);
-			typingIdx++;
-		} else {
-			if (liIndex >= liLength - 1) {
-				liIndex = 0;
-			} else {
-				liIndex++;
-			}
+  function typing() {
+    if (typingIdx < typingTxt.length) {
+      $(".typing").append(typingTxt[typingIdx]);
+      typingIdx++;
+    } else {
+      if (liIndex >= liLength - 1) {
+        liIndex = 0;
+      } else {
+        liIndex++;
+      }
 
-			typingIdx = 0;
-			typingBool = false;
-			typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
+      typingIdx = 0;
+      typingBool = false;
+      typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
 
-			clearInterval(tyInt);
-			setTimeout(function () {
-				$(".typing").html('&nbsp');
-				tyInt = setInterval(typing, 100);
-			}, 1000);
-		}
-	}
+      clearInterval(tyInt);
+      setTimeout(function () {
+        $(".typing").html("&nbsp");
+        tyInt = setInterval(typing, 100);
+      }, 1000);
+    }
+  }
 });
 
-
 (function ($) {
-	"use strict";
+  "use strict";
 
-	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: (target.offset().top - 72)
-				}, 1000, "easeInOutExpo");
-				return false;
-			}
-		}
-	});
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 72,
+          },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
+      }
+    }
+  });
 
-	$('.js-scroll-trigger').click(function () {
-		$('.navbar-collapse').collapse('hide');
-	});
+  $(".js-scroll-trigger").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 
-	$('body').scrollspy({
-		target: '#mainNav',
-		offset: 75
-	});
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 75,
+  });
 
-	var navbarCollapse = function () {
-		if ($("#mainNav").offset().top > 100) {
-			$("#mainNav").addClass("navbar-scrolled");
-		} else {
-			$("#mainNav").removeClass("navbar-scrolled");
-		}
-	};
-	navbarCollapse();
+  var navbarCollapse = function () {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-scrolled");
+    } else {
+      $("#mainNav").removeClass("navbar-scrolled");
+    }
+  };
+  navbarCollapse();
 
-	$(window).scroll(navbarCollapse);
+  $(window).scroll(navbarCollapse);
 
-	$('#about').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0, 1]
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-		}
-	});
+  $("#about").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1],
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    },
+  });
 })(jQuery);
